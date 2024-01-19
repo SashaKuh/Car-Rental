306f3e162a3f37c5c5423d7ece93cb1a5ae092c2
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2024-01-20 01:20:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { createRoot } from 'react-dom/client';
+
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
@@ -7,12 +9,13 @@ import { GlobalStyle } from 'components/GlobalStyles';
 
 const root = document.getElementById('root');
 
-createRoot(root).render(
-  
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
   <Provider store={store}>
     <GlobalStyle />
-    <BrowserRouter basename="/car-rental">
+    <BrowserRouter basename='/car-rental'>
       <App />
     </BrowserRouter>
-  </Provider>
+    </Provider>
+    </React.StrictMode>
 );
