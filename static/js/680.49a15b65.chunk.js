be108f5a74591e197ce24ec0d10b2ccab7e075ca
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2024-01-19 09:18:00 +0000

    Deploying to gh-pages from @ SashaKuh/car-rental@56ed01b5884e295598f4e32bb2c3112089333cb9 🚀

---
+++ static/js/680.49a15b65.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcar_rental=self.webpackChunkcar_rental||[]).push([[680],{680:(e,a,c)=>{c.r(a),c.d(a,{default:()=>r});const r=()=>{}}}]);
+//# sourceMappingURL=680.49a15b65.chunk.js.map
